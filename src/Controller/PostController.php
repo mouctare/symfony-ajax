408d1ad161c0874eeaf1e72fbd799ef779d305ed
mouctare <mouctard78@gmail.com>
2020-04-28 17:27:29 +0200

    AddModiff

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
-use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+ use App\Entity\PostLike;
+ use App\Repository\PostRepository;
+
+use App\Entity\Post;
+
+use App\Repository\PostLikeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
@@ -17,5 +26,27 @@ class PostController extends AbstractController
             'posts' => $repo->findAll(),
         ]);
     
+    }
+
+    /**
+     * permet de liker ou unliker un article
+     * 
+     * @Route("/post/{id}/like", name="post_like")
+     
+     
+     */
+    public function like(Post $post, EntityManagerInterface $manager,PostLikeRepository $likeRepo ) :  Response 
+   
+    {
+        return $this->json([ 'code' => 200, 
+        'message' => 'Votre avez bien liké cet article',
+        'like' => $likeRepo->count(['post' => $post])
+    
+    
+    ], 200);
+
+
+
+        
     }
 }
