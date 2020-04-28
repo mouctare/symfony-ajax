b6666bf9431a56f54a6fbea8e5badc0a54abbba2
mouctare <mouctard78@gmail.com>
2020-04-28 11:31:24 +0200

    AddUsers Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,14 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Post;
 use App\Entity\User;
+use App\Entity\PostLike;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Faker\Factory;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 
@@ -32,12 +34,31 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
         $faker = Factory::create();
+        $user = []; // vue qu'on a plusieurs utilisateurs on les mets dans un tableau
 
         $user = new User();
         $user->setEmail('user@symfony.com')
             ->setPassword($this->encoder->encodePassword($user, 'password'));
 
         $manager->persist($user);
+        $users[] = $user;
+
+
+
+        for ($i = 0; $i < 20; $i++){
+             $user = new User(); // on crée un utilisateur à, chaque fois qu'on passe dans cette boucle.
+            $user->setEmail($faker->email)
+            ->setPassword($this->encoder->encodePassword($user, 'password'));
+               
+
+            $manager->persist($user);
+            $users[] = $user;
+
+
+
+        }
+
+
 
         for ($i = 0; $i < 20; $i++) {
             $post = new Post();
@@ -46,10 +67,18 @@ class AppFixtures extends Fixture
                 ->setContent('<p>' . join(',', $faker->paragraphs()) . '</p>');
 
             $manager->persist($post);
+
+            for ($j = 0; $j < mt_rand(0, 10); $j++) {
+                $like = new PostLike();
+                $like->setPost($post)
+                    ->setUser($faker->randomElement($users));
+
+                    $manager->persist($like);
+            }
         }
 
         $manager->flush();
 
-        $manager->flush();
+    
     }
 }
